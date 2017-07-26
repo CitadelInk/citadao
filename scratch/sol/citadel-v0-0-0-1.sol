24029852850a79ee62753eadb9bbad51992b2a0e
nickreynolds <nicholas.s.reynolds@gmail.com>
2017-07-26 11:39:50 -0700

    updating contract structs and submission functions for bio and regular post.

--- scratch/sol/citadel-v0-0-0-1.sol
+++ scratch/sol/citadel-v0-0-0-1.sol
@@ -1,61 +1,51 @@
 pragma solidity ^0.4.11;
-
-
 contract Citadelv01 {
+    address public citadel_comptroller;
     
-
-	address public citadel_comptroller;
-    
-    
-	struct Authorg {
+    struct Authorg {
+        Revision[] selfBioRevisions;
         
-		bytes32[] selfBioRevisions;
-        
-		mapping(bytes32 => Submission) submissions;
-
-    	}
+        mapping(bytes32 => Submission) submissions;
+        bytes32[] allAuthorgSubmissionHashes;
+    }
     
+    struct Submission {
+        Revision[] pageSubmissionRevisions;
+    }
     
-
-	struct Page {
+    struct Revision {
+        bytes32 citadelManifestHash;
+        bytes32[] responseManifestHashes;
+    }
+    
+    mapping(address => Authorg) internalAuthorgs;
+    
+    function submitBioRevision(bytes32 citadelManifestHash) {
+        var newResponseArray = new bytes32[](0);
+        internalAuthorgs[msg.sender].selfBioRevisions.push(Revision({
+            citadelManifestHash : citadelManifestHash,
+            responseManifestHashes : newResponseArray
+        }));
+    }
+    
+    function isAuthorOfSubmission(address authorgAddress, bytes32 submissionHash) returns (bool isAuthor) {
+        var authorg = internalAuthorgs[authorgAddress];
+        var submission = authorg.submissions[submissionHash];
+        return submission.pageSubmissionRevisions.length != 0;
+    }
+    
+    function submitRevision(bytes32 subCitadelManifestHash, bytes32 revCitadelManifestHash) {
+        internalAuthorgs[msg.sender].allAuthorgSubmissionHashes.push(subCitadelManifestHash);
+        var newResponseArray = new bytes32[](0);
+        var revisionArray = internalAuthorgs[msg.sender].submissions[subCitadelManifestHash].pageSubmissionRevisions;
+        revisionArray.push(Revision({
+            citadelManifestHash : revCitadelManifestHash,
+            responseManifestHashes : newResponseArray
+        }));
         
-		Submission[] pageSubmissionRevisions;
-
-	}
-
-	struct Submission {
-        
-		bytes32 citadelManifestHash;
-
-        	//bytes32[] responseManifestHashes;
-
-    	}
-
-
-	
-mapping(address => Authorg) internalAuthorgs;
-
-	
-function submitBio(bytes32 citadelManifestHash) {
-
-		internalAuthorgs[msg.sender].submissions[citadelManifestHash] = Submission({
-
-			citadelManifestHash : citadelManifestHash
-
-		});
-
-	}
-
-
-	
-function isAuthorOfSubmission(address authorgAddress, bytes32 submissionHash) returns (bool isAuthor) {
-
-		var authorg = internalAuthorgs[authorgAddress];
-
-		var submission = authorg.submissions[submissionHash];
-
-		return submission.citadelManifestHash == submissionHash;
-
-	}
-
+    }
+    
+    function respondToBio(address authorgAddress, bytes32 submissionHash, bytes32 responseHash) {
+        //submit(submissionHash);
+    }
 }
