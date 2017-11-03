9171759f4d15f8ebf3fd0df3304932782af90571
aspiering <aspiering@gmail.com>
2017-11-02 17:23:53 -0700

    updating truffle libs

--- src/app-contracts/truffle.js
+++ src/app-contracts/truffle.js
@@ -4,7 +4,8 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id
+      gas: 4612388
     },
     production: {
       host: '104.236.160.22',
