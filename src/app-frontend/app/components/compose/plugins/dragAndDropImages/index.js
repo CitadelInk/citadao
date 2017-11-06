6b0cdb674e138d0c47f63080590ca702588bffae
nickreynolds <nicholas.s.reynolds@gmail.com>
2017-11-05 18:15:11 -0800

    removing is-image package that was causing issues on production somehow

--- src/app-frontend/app/components/compose/plugins/dragAndDropImages/index.js
+++ src/app-frontend/app/components/compose/plugins/dragAndDropImages/index.js
@@ -1,6 +1,9 @@
-
 import Promise from 'es6-promise'
-import isImage from 'is-image'
+import path from 'path';
+
+import imageExtensions from 'image-extensions';
+const exts = new Set(imageExtensions);
+
 import isUrl from 'is-url'
 import logger from 'slate-dev-logger'
 import mime from 'mime-types'
@@ -13,14 +16,12 @@ import { getEventTransfer } from 'slate-react'
  *
  * @param {Object} options
  *   @property {Function} insertImage
- *   @property {Array} extensions (optional)
  * @return {Object} plugin
  */
 
 function DropOrPasteImages(options = {}) {
   let {
-    insertImage,
-    extensions,
+    insertImage
   } = options
 
   if (options.applyTransform) {
@@ -44,9 +45,6 @@ function DropOrPasteImages(options = {}) {
   function asyncApplyChange(change, editor, file) {
     return Promise
       .resolve(insertImage(change, file, editor))
-      //.then(() => {
-      //  editor.onChange(change)
-      //})
   }
 
 
@@ -79,13 +77,13 @@ function DropOrPasteImages(options = {}) {
    */
 
   function onInsertFiles(event, change, editor, transfer) {
+    console.warn("on insert files");
     const { target, files } = transfer
 
     for (const file of files) {
-      if (extensions) {
-        const ext = mime.extension(file.type)
-        if (!extensions.includes(ext)) continue
-      }
+      var split = file.type.split('/')
+      console.warn("file.type.split('/'): " + split)
+      if (split[0] !== "image" || !exts.has(split[1])) return
 
       if (target) {
         change.select(target)
@@ -108,6 +106,8 @@ function DropOrPasteImages(options = {}) {
    */
 
   function onInsertHtml(event, change, editor, transfer) {
+    
+    console.warn("onInsertHtml");
     const { html, target } = transfer
     const parser = new DOMParser()
     const doc = parser.parseFromString(html, 'text/html')
@@ -117,10 +117,7 @@ function DropOrPasteImages(options = {}) {
 
     const src = firstChild.src
 
-    if (extensions) {
-      const ext = extname(src).slice(1)
-      if (!extensions.includes(ext)) return
-    }
+    if (!exts.has(path.extname(src).slice(1).toLowerCase())) return
 
     loadImageFile(src, (err, file) => {
       if (err) return
@@ -145,7 +142,7 @@ function DropOrPasteImages(options = {}) {
   function onInsertText(event, change, editor, transfer) {
     const { text, target } = transfer
     if (!isUrl(text)) return
-    if (!isImage(text)) return
+    if (!exts.has(path.extname(text).slice(1).toLowerCase())) return
 
     loadImageFile(text, (err, file) => {
       if (err) return
